20b9ddbff67ae4c177fc72f9414ac2ce66b99162
Ali Biner <alibiner.github@gmail.com>
2025-07-06 16:58:02 +0300

    create short max to min algorithm

--- src/main/java/com/alibiner/Main.java
+++ src/main/java/com/alibiner/Main.java
@@ -1,7 +1,28 @@
 package com.alibiner;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[3];
 
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Enter " + (i+1) + ". number: ");
+            numbers[i] = scanner.nextInt();
+        }
+
+        for (int i = numbers.length-1; i >=0; i--) {
+            for (int j = i-1; j >=0 ; j--) {
+                if (numbers[j] < numbers[i]){
+                    int temp = numbers[j];
+                    numbers[j] = numbers[i];
+                    numbers[i] = temp;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(numbers));
     }
 }
